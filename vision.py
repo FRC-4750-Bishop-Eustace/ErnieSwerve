8baac7d5113c12e2b63ff947664c68a0e9a5877f
Shawndod3 <157923705+Shawndod3@users.noreply.github.com>
2024-03-08 18:36:48 -0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -15,4 +15,10 @@ upper_orange=np.array([255,165,0])
 frame_hsv = cv2.cvtColor(frame,cv2.COLOR_RGB2H5V)
 frame_inrange = cv2.inrange(frame_hsv,lower_orange,upper_orange)
 cv2.imshow("Camera Stream", frame_inrange)
+
+
+cv2.findCountours(frame_inrange, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+   img=cv2.drawContours(frame, [cnt], -1, (255, 0, 0), 2)
+cv2.imshow("CameraStream",img)
 cv2.waitKey(0)
