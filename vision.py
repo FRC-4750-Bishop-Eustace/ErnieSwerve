cb261b1a5bb497e01a963547900c101afba1d6c5
Shawndod3 <157923705+Shawndod3@users.noreply.github.com>
2024-03-09 12:52:01 -0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,6 +1,21 @@
 import cv2
 import numpy as np
+from cscore import CameraServer
+import cv2
+import numpy as np
 
+CameraServer.enableLogging()
+
+camera = CameraServer.startAutomaticCapture()
+camera.setResolution(width, height)
+
+sink = cs.getVideo()
+
+while True:
+   time, input_img = cvSink.grabFrame(input_img)
+
+   if time == 0: # There is an error
+      continue
 frame_hsv = cv2.cvtColor(frame,cv2.COLOR_RGB2H5V)
 frame_inrange = cv2.inrange(frame_hsv,lower_orange,upper_orange)
 cv2.imshow("Camera Stream", frame_inrange)
