002c6fcb6a76b7c4b475ffe5b97bdfd711f83b81
Shawndod3 <157923705+Shawndod3@users.noreply.github.com>
2024-03-09 11:10:38 -0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,18 +1,3 @@
-import cv2
-import numpy as np
-
-cap=cv2.VideoCapture(0)
-while True:
-   #_ is a throwaway variable
-   _,frame=cap.read()
-   cv2.imshow("Camera Stream".frame)
-   cv2.waitKey(1)
-
-frame=cv2.imread("output.jpg")
-cropped_img=frame[0:int(frame.shape[0]/2)]
-lower_orange=np.array([255,75,0])
-upper_orange=np.array([255,165,0])
-
 frame_hsv = cv2.cvtColor(frame,cv2.COLOR_RGB2H5V)
 frame_inrange = cv2.inrange(frame_hsv,lower_orange,upper_orange)
 cv2.imshow("Camera Stream", frame_inrange)
